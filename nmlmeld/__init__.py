45ef7d5e745563cdaf93b3dd0719625a4f950c66
James Harle <jdha@noc.ac.uk>
2021-05-17 11:11:21 +0100

    update files for typos

--- nmlmeld/__init__.py
+++ nmlmeld/__init__.py
@@ -1,7 +1,7 @@
 from pkg_resources import DistributionNotFound, get_distribution
 
 try:
-    __version__ = get_distribution("pydomcfg").version
+    __version__ = get_distribution("nmlmeld").version
 except DistributionNotFound:
     # package is not installed
     __version__ = "unknown"
